b8fa25509b10a33f9740973cea4f680ce1a50912
hecano17 <hectorvillavicencio@gmail.com>
2019-08-30 13:11:51 -0400

    Se Agrega Carro Completo y Modulo Admin

--- src/app/modelo/modelo.module.ts
+++ src/app/modelo/modelo.module.ts
@@ -6,13 +6,15 @@ import { Orden } from './orden.model';
 import { OrdenRepositorio } from './orden.repositorio';
 import { HttpClientModule } from '@angular/common/http';  
 import { RestDataSource } from './rest.datasource';
+import { AuthService } from './auth.service';
 
 @NgModule(
     {
         imports:[HttpClientModule],
         providers:[StaticDataSource,ProductoRepositorio, Carro,
          Orden,OrdenRepositorio,
-        {provide: StaticDataSource, useClass: RestDataSource}
+        {provide: StaticDataSource, useClass: RestDataSource},
+        AuthService, RestDataSource
         ]
 
     }
