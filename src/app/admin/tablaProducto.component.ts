ea3cb3b088873ca6623d56d0c8b5f4120008c65e
hecano17 <hectorvillavicencio@gmail.com>
2019-09-03 09:33:02 -0400

    Version Penultima

--- src/app/admin/tablaProducto.component.ts
+++ src/app/admin/tablaProducto.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductoRepositorio } from '../modelo/producto.repositorio';
 
 @Component({
-    template:`<div class="bg-info p-2 text-white"><h3>Tabla de Productos</h3></div>`
+   // template:`<div class="bg-info p-2 text-white"><h3>Tabla de Productos</h3></div>`
+   templateUrl: "tablaProducto.component.html"
 })
 export class TablaProductoComponent{
+
+    constructor(private repositorio: ProductoRepositorio){
+
+    }
 
 }
