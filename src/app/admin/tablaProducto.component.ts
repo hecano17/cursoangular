d121b21a33f97afe6be66975e18cf8f9b8f4e624
hecano17 <hectorvillavicencio@gmail.com>
2019-09-03 09:41:42 -0400

    Version Penultima

--- src/app/admin/tablaProducto.component.ts
+++ src/app/admin/tablaProducto.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductoRepositorio } from '../modelo/producto.repositorio';
+import { Producto } from '../modelo/producto.model';
 
 @Component({
    // template:`<div class="bg-info p-2 text-white"><h3>Tabla de Productos</h3></div>`
@@ -9,6 +10,14 @@ export class TablaProductoComponent{
 
     constructor(private repositorio: ProductoRepositorio){
 
+    }
+
+    getProductos():Producto[]{
+        return this.repositorio.getProductos();
+    }
+
+    eliminarProducto(id: number){
+        this.repositorio.eliminarProducto(id);
     }
 
 }
